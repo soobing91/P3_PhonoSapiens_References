fe15158579d4dbd24a7b6e07b381b7814ef3c140
SeunghwaJun <bestest106@gmail.com>
2019-10-05 13:18:48 -0400

    SHJ Rererence List

--- paul_code/csv_to_json.py
+++
@@ -1,8 +0,0 @@
-import csv
-import json
-
-file = open("../Data/refCSVpd.csv")
-reader = csv.DictReader( file, fieldnames = ( "Institution","Reference_Count" ))  
-out = json.dumps( [ row for row in reader ] )  
-file2 = open( 'static/data/data.json', 'w')  
-file2.write(out)  
--- paul_code/csv_to_json_full.py
+++
@@ -1,8 +0,0 @@
-import csv
-import json
-
-file = open("../Data/refCSVpdFULL.csv")
-reader = csv.DictReader( file, fieldnames = ("Part_Count","Chapter_Count","Page_Number","Institution","Language","Keyword","URL" ))  
-out = json.dumps( [ row for row in reader ] )  
-file2 = open( 'static/data/dataFULL.json', 'w')  
-file2.write(out) 
